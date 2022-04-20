be550665861dce589c2206dfe0e2b900646118d7
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-04-20 15:19:31 -0500

    5 se exporto el archivo mc de la carpeta app al index.js y se modificaron los comandos de node

--- app/missionCommander.js
+++ app/missionCommander.js
@@ -1,4 +1,4 @@
-class MisionComander{
+class MissionCommander{
 
 	constructor(name){
 	this.name = name
---
+++ index.js
@@ -0,0 +1,9 @@
+
+const MissionCommander = require('./app/missionCommander');
+const daniel = new MissionCommander("Daniel")
+console.log(daniel.name)
+
+
+
+
+
