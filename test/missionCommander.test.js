8f443c97072416e95416ee358c52e0007deea9c0
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-04-20 16:03:13 -0500

    6 Se instalo jest se creo el documento mc.t.js y se hicieron las pruebas señaladas

---
+++ test/missionCommander.test.js
@@ -0,0 +1,12 @@
+describe("Esto es una suite de pruebas",()=>{
+
+		test('Caso de prueba 1',() =>{
+
+
+		const result = 1 + 2
+		expect(result).toBe(3);
+		//expect(result).toBe(10);
+    });
+
+
+})
